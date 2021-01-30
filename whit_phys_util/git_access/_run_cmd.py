0eed96e6dd5270a20796fed83ad94452cf05088e
John Larkin <jlarkin@whitworth.edu>
2021-01-29 16:49:45 -0800

    More output from run_secret_cmd

--- whit_phys_util/git_access/_run_cmd.py
+++ whit_phys_util/git_access/_run_cmd.py
@@ -12,12 +12,12 @@ def run_secret_cmd(cmd):
     Runs a command using subprocess.run with some error checking, but no command echo to
     to avoid revealing secret info.
     """
-    try:
-        cp = subprocess.run(cmd, shell=True)
-        return cp.returncode
-    except Exception as exc:
-        logger.error("\n%s", exc)
-        return cp.returncode
+    cp = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    if (cp.returncode):
+        print("returncode: ", cp.returncode)
+        print("stdout: ", cp.stdout)
+        print("stderr: ", cp.stderr)
+    return cp.returncode
 
 def run_cmd(cmd):
     """run_cmd
