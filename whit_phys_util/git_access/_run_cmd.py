63d313db154fcc9b1d925fefdb55a1adbc981d09
John Larkin <jlarkin@whitworth.edu>
2021-01-29 21:05:16 -0800

    Differentiate run_cmd error and success output

--- whit_phys_util/git_access/_run_cmd.py
+++ whit_phys_util/git_access/_run_cmd.py
@@ -1,33 +1,33 @@
 """Run cmd in subprocess.run.
 
-Some error checking is performed with goal of avoiding echo of secret info.
+Some error logging is performed with goal of avoiding echo of secret info.
 """
 import subprocess
-import shlex
 from logzero import logger
 
 def run_secret_cmd(cmd):
     """run_secret_cmd
 
-    Runs a command using subprocess.run with some error checking, but no command echo to
+    Runs a command using subprocess.run with some error logging, but no command echo to
     to avoid revealing secret info.
     """
     cp = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     if (cp.returncode):
-        print("returncode: ", cp.returncode)
-        print("stdout: ", cp.stdout)
-        print("stderr: ", cp.stderr)
+        logger.error("%s", cp.stderr)
+    else:
+        print(cp.stdout)
     return cp.returncode
 
 def run_cmd(cmd):
     """run_cmd
 
-    Runs a command using subprocess.run with some error checking
+    Runs a command using subprocess.run with some error logging
     """
-    try:
-        cp = subprocess.run(cmd, shell=True)
-        return cp.returncode
-    except Exception as exc:
-        logger.info("%s", cmd)
-        logger.error("\n%s", exc)
-        return True
+    cp = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    if (cp.returncode):
+        logger.info("%s", cp.args)
+        logger.error("%s", cp.stderr)
+    else:
+        print(cp.stdout)
+    return cp.returncode
+
