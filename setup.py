b0dd3ce5397943fd41c9fd21abb2730733536076
John Larkin <jlarkin@whitworth.edu>
2021-01-31 13:15:43 -0800

    setup.py wasn't staged

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("version.txt", "r", encoding="utf-8") as fh:
+with open("whit_phys_util/version.txt", "r", encoding="utf-8") as fh:
     version = fh.read()
 
 setuptools.setup(
