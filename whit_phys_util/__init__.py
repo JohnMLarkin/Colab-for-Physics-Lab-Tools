0718d525134af80ae337669470ad0dfae40d7ebb
John Larkin <jlarkin@whitworth.edu>
2021-01-30 16:26:49 -0800

    Remove VERSION

--- whit_phys_util/__init__.py
+++ whit_phys_util/__init__.py
@@ -1,12 +1,12 @@
 # __init__.py for whit-phys-utils
 
-name = "whit_phys_utils"
-with open("../version.txt", "r", encoding="utf-8") as fh:
-    __version__ = fh.read()
+#name = "whit_phys_utils"
+#with open("../version.txt", "r", encoding="utf-8") as fh:
+#    __version__ = fh.read()
 
-VERSION = __version__.split(".")
+#VERSION = __version__.split(".")
 
 from .git_access import *
 from .pretty_fit import fitTable, prettyPolyFit
 
-__all__ = ["__version__","local_repository", "fitTable", "prettyPolyFit"]
+__all__ = ["local_repository", "fitTable", "prettyPolyFit"]
